a6bccb8a709f49b265de66a74ed5aa53103c7741
jiwon lee <zest1923@gmail.com>
2022-07-16 16:42:06 +0900

    [Init] 프로젝트 초기 설정

---
+++ APillLog/APillLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APillLog
+//
+//  Created by 이지원 on 2022/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
