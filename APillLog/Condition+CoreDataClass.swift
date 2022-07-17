e92756a10e58b2c1ce59e219c3c09d7a0d81d383
mycookie1 <103025266+mycookie1@users.noreply.github.com>
2022-07-17 14:52:09 +0900

    [Feat] #2 - 코어데이터 모델 추가
    7개 모델추가

---
+++ APillLog/Condition+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  [String]+CoreDataClass.swift
+//  APillLog
+//
+//  Created by 종건 on 2022/07/17.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Condition)
+public class Condition: NSManagedObject {
+
+}
---
+++ APillLog/Condition+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  [String]+CoreDataProperties.swift
+//  APillLog
+//
+//  Created by 종건 on 2022/07/17.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Condition {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Condition> {
+        return NSFetchRequest<Condition>(entityName: "Condition")
+    }
+
+    @NSManaged public var createTime: Date?
+    @NSManaged public var detailContext: String?
+    @NSManaged public var dosage: String?
+    @NSManaged public var id: UUID?
+    @NSManaged public var medicinalEffect: [String]?
+    @NSManaged public var name: String?
+    @NSManaged public var sideEffect: [String]?
+
+}
+
+extension Condition : Identifiable {
+
+}
