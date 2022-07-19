d686421e8a02987e65b4041ea99b30df1f4da2ae
sm-amoled <moktak072@naver.com>
2022-07-20 02:28:49 +0900

    [Feat] #37 - ManageDosingView 테이블뷰 동작 구현
    - dummy 데이터를 이용해 DataSource, Delegate 구현

--- APillLog/APillLog/View/MedicationTab/ManageDosingView/ManageDosingViewController.swift
+++ APillLog/APillLog/View/MedicationTab/ManageDosingView/ManageDosingViewController.swift
@@ -13,29 +13,92 @@ class ManageDosingViewController: UIViewController {
     // MARK: @IBOutlet
     @IBOutlet weak var viewTitle: UILabel!
     @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     
     // MARK: Property
+    let cellIdentifier = "ManageDosingCell"
+    let dummies: [dummy] = [
+        dummy(name: "콘서타", dosage: "18mg", dosingCycle: 3, isShowing: true, createTime: Date()),
+        dummy(name: "메디피진", dosage: "10mg", dosingCycle: 1, isShowing: true, createTime: Date()),
+        dummy(name: "인데졸정", dosage: "", dosingCycle: 6, isShowing: true, createTime: Date()),
+        dummy(name: "콘서타", dosage: "18mg", dosingCycle: 4, isShowing: true, createTime: Date())
     
+    ]
     
     // MARK: LifeCycle Function
     override func viewDidLoad() {
-        
+        viewTitle.font = UIFont.AFont.navigationTitle
     }
 }
 
 extension ManageDosingViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return self.dummies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell: ManageDosingTableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ManageDosingTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.dosingCycleLabel.font = UIFont.AFont.caption
+        cell.nameAndDosageLabel.font = UIFont.AFont.tableViewBody
+        
+        cell.dosingCycleLabel.textColor = UIColor.AColor.gray
+        cell.nameAndDosageLabel.textColor = UIColor.AColor.black
+        
+        cell.nameAndDosageLabel.text = dummies[indexPath.row].nameAndDosage()
+        cell.dosingCycleLabel.text = dummies[indexPath.row].dosingCycleToString()
+        
+        return cell
     }
-    
-    
 }
 
 extension ManageDosingViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        65
+    }
 }
 
+// ManageDosingTableViewCell
+class ManageDosingTableViewCell: UITableViewCell {
+    // MARK: @IBOutlet
+    @IBOutlet weak var dosingCycleLabel: UILabel!
+    @IBOutlet weak var nameAndDosageLabel: UILabel!
+    @IBOutlet weak var isShowingSwitch: UISwitch!
+}
+
+struct dummy {
+    let name: String
+    let dosage: String
+    let dosingCycle: Int
+    let isShowing: Bool
+    let createTime: Date
+    
+    func nameAndDosage() -> String {
+        return name + " " + dosage
+    }
+    
+    func dosingCycleToString() -> String {
+        var str = ""
+        var cycle = dosingCycle
+        
+        if dosingCycle % 2 == 1 {
+            str.append("아침")
+        }
+        
+        cycle = cycle / 2
+        if dosingCycle % 2 == 1 {
+            if !(str.isEmpty) { str.append(", ") }
+            str.append("점심")
+        }
+        
+        cycle = cycle / 2
+        if dosingCycle % 2 == 1 {
+            if !(str.isEmpty) { str.append(", ")}
+            str.append("저녁")
+        }
+        
+        return str
+    }
+}
