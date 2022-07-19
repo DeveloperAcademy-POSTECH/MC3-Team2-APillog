608e6fe0a8c2a30239ae84bc124a6d21b1372183
sm-amoled <moktak072@naver.com>
2022-07-19 21:07:07 +0900

    [Style] #30 - 폰트 및 컬러 extension 추가
    - UIFont, UIColor Extension 추가로 코드로 색상 사용 가능
    - import한 폰트파일이 이미 import 되어있어 하나 삭제함

--- APillLog/APillLog/View/MedicationTab/MedicationViewController.swift
+++ APillLog/APillLog/View/MedicationTab/MedicationViewController.swift
@@ -8,11 +8,9 @@
 import UIKit
 
 class MedicationViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
