f60d950d9dc28482d8bfb72dd6cde69ee0f7ae04
jiwon lee <zest1923@gmail.com>
2022-07-20 23:03:26 +0900

    [Feat] #23 - CalendarView 구현 - Label 클릭 시 DatePicker 생성 - Prototype에 맞도록 디자인 수정

---
+++ APillLog/APillLog/Utility/Calendar/CalendarView.swift
@@ -0,0 +1,84 @@
+//
+//  CalendarView.swift
+//  APillLog
+//
+//  Created by 이지원 on 2022/07/20.
+//
+
+import UIKit
+
+class CalendarView: UIView {
+    
+    @IBOutlet weak var selectedDate: UILabel!
+    
+    let datePicker: UIDatePicker = {
+        
+        let datePicker = UIDatePicker()
+        datePicker.locale = .current
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .inline
+        datePicker.backgroundColor = .systemBackground
+        datePicker.tintColor = .systemGreen
+        datePicker.layer.cornerRadius = 10
+        datePicker.layer.masksToBounds = true
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        
+        return datePicker
+    }()
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+        customInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    func commonInit() {
+        let view = Bundle.main.loadNibNamed("CalendarView", owner: self, options: nil)?.first as! UIView
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+    func customInit() {
+        
+        guard let view = Bundle.main.loadNibNamed("CalendarView", owner: self, options: nil)?.first as? UIView else { return }
+            view.frame = self.bounds
+            self.addSubview(view)
+        
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM월 dd일 E요일"
+        selectedDate.text = formatter.string(from: date)
+        
+        let gestureRecognize = UITapGestureRecognizer(target: self, action: #selector(labelClicked))
+        selectedDate.addGestureRecognizer(gestureRecognize)
+        selectedDate.isUserInteractionEnabled = true
+    }
+    
+    // MARK: DatePicker func
+    func setDatePickerConstraints() {
+        self.datePicker.topAnchor.constraint(equalTo: self.topAnchor, constant: 60).isActive = true
+        self.datePicker.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    }
+    
+    @objc private func labelClicked(_ tapRecognizer: UITapGestureRecognizer) {
+        
+        print("Hello \(datePicker.date)")
+        superview!.addSubview(datePicker)
+        self.datePicker.isHidden = false
+        setDatePickerConstraints()
+        datePicker.addTarget(self, action: #selector(datePickerValueChanged(sender:)), for: UIControl.Event.valueChanged)
+        datePicker.center = superview!.center
+    }
+    
+    @objc private func datePickerValueChanged(sender: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat =  "MM월 dd일 E요일"
+        selectedDate.text = formatter.string(from: sender.date)
+        self.datePicker.isHidden = true
+    }
+}
+
