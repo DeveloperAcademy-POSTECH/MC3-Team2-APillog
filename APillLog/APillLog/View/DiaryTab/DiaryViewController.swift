ba3bbfc60984a56227632513774dc29d514ae51c
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-07-20 15:18:55 +0900

    [Feat] #24 - TextView 수정 & 확정
    * 키보드의 존재로 인해 텍스트 뷰의 크기를 줄였다. 이로인해 생기는 공백은 사용자가 페이지에 진입하면 자동으로 텍스트뷰에 포커싱을 주어 키보드의 존재로 대체하였다.

--- APillLog/APillLog/View/DiaryTab/DiaryViewController.swift
+++ APillLog/APillLog/View/DiaryTab/DiaryViewController.swift
@@ -10,33 +10,19 @@ import UIKit
 class DiaryViewController: UIViewController {
 
 
-    @IBOutlet var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // call the 'keyboardWillShow' function when the view controller receive the notification that a keyboard is going to be shown
-               NotificationCenter.default.addObserver(self, selector: #selector(DiaryViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-
-               // call the 'keyboardWillHide' function when the view controlelr receive notification that keyboard is going to be hidden
-               NotificationCenter.default.addObserver(self, selector: #selector(DiaryViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-       
-    
-        // Do any additional setup after loading the view.
+//        // call the 'keyboardWillShow' function when the view controller receive the notification that a keyboard is going to be shown
+//               NotificationCenter.default.addObserver(self, selector: #selector(DiaryViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+//
+//               // call the 'keyboardWillHide' function when the view controlelr receive notification that keyboard is going to be hidden
+//               NotificationCenter.default.addObserver(self, selector: #selector(DiaryViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+//
+//
+//        // Do any additional setup after loading the view.
     }
-    @objc func keyboardWillShow(notification: NSNotification) {
-                
-            guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-               // if keyboard size is not available for some reason, dont do anything
-               return
-            }
-          
-          // move the root view up by the distance of keyboard height
-          self.view.frame.origin.y = 0 - keyboardSize.height
-        }
-
-        @objc func keyboardWillHide(notification: NSNotification) {
-          // move back the root view origin to zero
-          self.view.frame.origin.y = 0
-        }
+    
 
     
     /*
