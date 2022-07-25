9da8443a9caaf82973ec6b58b5449ca0143f12cb
yeni <yeniful@icloud.com>
2022-07-25 22:59:33 +0900

    [Fix] #83 - 네비게이션 버튼 설명 폰트 수정

--- APillLog/APillLog/Extentions/APillLog+Font.swift
+++ APillLog/APillLog/Extentions/APillLog+Font.swift
@@ -55,7 +55,7 @@ extension UIFont {
         static var navigationTitle: UIFont { UIFont.APillLogFont(forTextStyle: .title2) }
         static var datePickerText: UIFont { UIFont.APillLogFont(forTextStyle: .title2) }
         static var tableViewTitle: UIFont { UIFont.APillLogFont(forTextStyle: .title2) }
-        static var navigationButtonDescriptionLabel: UIFont { UIFont.APillLogFont(forTextStyle: .caption1) }
+        static var navigationButtonDescriptionLabel: UIFont { UIFont.APillLogFont(forTextStyle: .footnote) }
 
         static var cardViewTitle: UIFont { UIFont.APillLogFont(forTextStyle: .title3) }
         static var cardViewPillName: UIFont { UIFont.APillLogFont(forTextStyle: .title3) }
