f866b8d61bca0b739b41f3161b7e5a14db62f996
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-07-27 18:54:18 +0900

    [Feat] #105 WatchOS 초기 설정

---
+++ APillLog/APilLogWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  APilLogWatch WatchKit Extension
+//
+//  Created by 이영준 on 2022/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ APillLog/APilLogWatch WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  APilLogWatch WatchKit Extension
+//
+//  Created by 이영준 on 2022/07/27.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
