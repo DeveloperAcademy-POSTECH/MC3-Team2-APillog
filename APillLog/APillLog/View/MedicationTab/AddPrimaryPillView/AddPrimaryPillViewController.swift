d958a41644d698c2879b6698321918393cb5ae66
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-07-28 10:24:51 +0900

    [Feat] #113 약 용량 Segmented Control UI 수정 / 기능 구현

--- APillLog/APillLog/View/MedicationTab/AddPrimaryPillView/AddPrimaryPillViewController.swift
+++ APillLog/APillLog/View/MedicationTab/AddPrimaryPillView/AddPrimaryPillViewController.swift
@@ -25,11 +25,13 @@ class AddPrimaryPillViewController: UIViewController, UISheetPresentationControl
     @IBOutlet weak var cancleButton: UIButton!
     @IBOutlet weak var duplicateWarningLabel: UILabel!
     
+    @IBOutlet weak var primaryPillDosageSegmentedControl: UISegmentedControl!
     
     // MARK: Property
     var coredataManager:CoreDataManager = CoreDataManager()
     var primaryPillDosingCycle: Int = 0
     var primaryPillList: [PrimaryPill] = []
+    var primaryPillDosageSegmentedTitle = "mg"
     
     var delegate: AddPrimaryPillViewControllerDelegate?
     
@@ -55,15 +57,13 @@ class AddPrimaryPillViewController: UIViewController, UISheetPresentationControl
  
     @IBAction func tapSaveButton() {
         let pillName = PrimaryPillName.text ?? ""
-        let pillDosage = PrimaryPillDosage.text ?? ""
+        let pillDosage = (PrimaryPillDosage.text ?? "") + primaryPillDosageSegmentedTitle
         
         coredataManager.addPrimaryPill(name: pillName, dosage: pillDosage, dosingCycle: Int16(primaryPillDosingCycle))
+        print(pillDosage)
         delegate?.didAddPrimaryPill()
         self.presentingViewController?.dismiss(animated: true)
-       
-        
     }
-    
     
     @IBAction func detectNameTextField(){
         detectEnableSaveButton()
@@ -120,6 +120,16 @@ class AddPrimaryPillViewController: UIViewController, UISheetPresentationControl
         detectEnableSaveButton()
     }
     
+    @IBAction func selectPrimaryPillDosage(_ sender: UISegmentedControl) {
+        switch(primaryPillDosageSegmentedControl.selectedSegmentIndex) {
+        case 0:
+            primaryPillDosageSegmentedTitle = "mg"
+        case 1:
+            primaryPillDosageSegmentedTitle = "정"
+        default:
+            return
+        }
+    }
     
     // MARK: Function
     func changePrimaryPillDosingButtonState(_ button: UIButton) {
