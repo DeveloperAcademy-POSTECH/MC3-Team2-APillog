101178dec34dd8a34f50cbaf0e8448affb361c6e
mycookie1 <103025266+mycookie1@users.noreply.github.com>
2022-07-28 16:46:10 +0900

    [Feat] #124 알람페이지 연결
    Co-Authored-By: Jiwon <zest1923@gmail.com>

--- APillLog/APillLog/View/PreferencesTab/PushNotificationView/PushNotificationController.swift
+++ APillLog/APillLog/View/PreferencesTab/PushNotificationView/PushNotificationController.swift
@@ -4,5 +4,13 @@
 //
 //  Created by 이지원 on 2022/07/28.
 //
-
+import Foundation
 import UIKit
+
+class PushNotificationController: UIViewController{
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+        }
+}
+
