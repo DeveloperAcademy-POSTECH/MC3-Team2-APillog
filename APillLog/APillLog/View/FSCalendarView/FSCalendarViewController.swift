316a4a1a729a5b2110f4e38c9271f80ba57d934c
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-07-31 22:08:37 +0900

    [Feat] #146 - TableView Cell 크기에 따른 ScrollView 동적 변환
    TableView Height 는 ScrollView OFF

--- APillLog/APillLog/View/FSCalendarView/FSCalendarViewController.swift
+++ APillLog/APillLog/View/FSCalendarView/FSCalendarViewController.swift
@@ -18,7 +18,6 @@ class FSCalendarViewController: UIViewController{
     @IBOutlet var scrollBackgroundView: UIView!
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var calendarHeaderView: UIView!
-    
     private var currentPage: Date?
     
     private lazy var today: Date = {
@@ -42,6 +41,65 @@ class FSCalendarViewController: UIViewController{
         return dateFormatter
     }()
     
+    // History 변수
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var tableViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var backgroundViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var guideLabel: UILabel!
+    
+    var historyData = [History]()
+    var selectedDate: Date = Date()
+    
+    var guideLabelHidden = true {
+        didSet {
+            guideLabel.isHidden = guideLabelHidden
+        }
+    }
+    
+    var tableViewCellHeight: Int = 0
+    
+    func setUITableDelegate() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        historyData = CoreDataManager.shared.fetchHistory(selectedDate: Date())
+    }
+    
+    
+    private func resizingTableViewHeight(dataCount count: Double) {
+        if count == 0 {
+            tableViewHeight.constant = 66.0
+        } else {
+            tableViewHeight.constant = 91.0 * CGFloat(count)
+        }
+        
+        backgroundViewHeight.constant = CGFloat(tableViewHeight.constant) + 500
+    }
+    
+    func calculateTableViewCellHeight(_ date: Date) -> Double {
+        let historyArray = CoreDataManager.shared.fetchHistory(selectedDate: date)
+        var result: Double = 0
+        
+        for cell in historyArray {
+            if cell.pillName == nil || cell.pillName == "" {
+                
+            } else {
+                result += 1.0
+            }
+            if cell.sideEffect == nil || cell.sideEffect == [] {
+                
+            } else {
+                result += 1.0
+            }
+            if cell.detailContext == nil || cell.detailContext == "" {
+                
+            } else {
+                result += 0.8
+            }
+        }
+        return result
+    }
+    
     // MARK: View LifeCycle Function
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +107,8 @@ class FSCalendarViewController: UIViewController{
         self.scrollBackgroundView.backgroundColor = UIColor.AColor.background
         self.backgroundView.backgroundColor = UIColor.AColor.background
         self.calendarHeaderView.backgroundColor = UIColor.AColor.white
+        
+        self.guideLabel.font = UIFont.AFont.noHistory
         
         // delegation, datasource 할당
         setUpEvents()
@@ -60,13 +120,26 @@ class FSCalendarViewController: UIViewController{
         setCalendarStyle()
         
         self.headerLabel.font = UIFont.AFont.calenderText
+        
+        // History
+        setUITableDelegate()
+        registerNib()
+        self.tableView.layer.cornerRadius = 10
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         setCalendar()
-        // 확인 필요
-        calendarView.reloadData()
+        
+        historyData = CoreDataManager.shared.fetchHistory(selectedDate: Date())
+        tableView.reloadData()
+        
+        let cellCount = calculateTableViewCellHeight(Date())
+        resizingTableViewHeight(dataCount: cellCount)
+        
+        if historyData.count == 0 { guideLabelHidden = false }
+        else { guideLabelHidden = true }
     }
     
     override func viewWillLayoutSubviews() {
@@ -200,10 +273,91 @@ extension FSCalendarViewController: FSCalendarDelegate{
     // 날짜 선택 시 콜백 메소드
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         
+        historyData = CoreDataManager.shared.fetchHistory(selectedDate: date)
+        tableView.reloadData()
+        
+        // tableView Height
+        let totalCellCount = calculateTableViewCellHeight(date)
+        resizingTableViewHeight(dataCount: totalCellCount)
+        
+        if historyData.count == 0 { guideLabelHidden = false }
+        else { guideLabelHidden = true }
+        
     }
     
     // 날짜 선택 해제 시 콜백 메소드
     public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
        
     }
+}
+
+extension FSCalendarViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    private func registerNib() {
+        let nib = UINib(nibName: "HistoryTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "historyTableViewCell")
+    }
+    
+    // MARK: TableView Funcs
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        historyData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let dateformatter = DateFormatter()
+            dateformatter.dateFormat = "a hh:mm"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "historyTableViewCell", for: indexPath) as! HistoryTableViewCell
+        
+        cell.createdTime.text = dateformatter.string(from:historyData[indexPath.row].createTime ?? Date())
+        
+        if historyData[indexPath.row].pillName != nil {
+            cell.pillName.text = "💊  " + historyData[indexPath.row].pillName!
+        } else {
+            cell.pillName.text = historyData[indexPath.row].pillName
+        }
+        cell.sideEffect.text = historyData[indexPath.row].sideEffect?.joined(separator: " / ")
+        cell.detailContext.text = historyData[indexPath.row].detailContext
+
+        if cell.pillName.text == nil || cell.pillName.text == "" {
+            cell.stackViewPillName.isHidden = true
+            cell.pillLabelView.isHidden = true
+        } else {
+            cell.stackViewPillName.isHidden = false
+            cell.pillLabelView.isHidden = false
+            self.tableViewCellHeight += 1
+        }
+        if cell.sideEffect.text == nil || cell.sideEffect.text == "" {
+            cell.stackViewSideEffect.isHidden = true
+            cell.sideEffectLabelView.isHidden = true
+        } else {
+            cell.stackViewSideEffect.isHidden = false
+            cell.sideEffectLabelView.isHidden = false
+            self.tableViewCellHeight += 1
+        }
+        if cell.detailContext.text == nil || cell.detailContext.text == "" {
+            cell.stackViewDetailContext.isHidden = true
+            cell.detailContextLabelView.isHidden = true
+        } else {
+            cell.stackViewDetailContext.isHidden = false
+            cell.detailContextLabelView.isHidden = false
+            self.tableViewCellHeight += 1
+        }
+        
+        return cell
+    }
+    
+    private func fetchPillNameText(index: Int) -> String? {
+        if let pillName = historyData[index].pillName {
+            return pillName + historyData[index].dosage!
+        }
+        
+        if let pillName = historyData[index].names {
+            let zipped = zip(pillName, historyData[index].dosages!).map{ "\($0) \($1)"  }
+            return zipped.joined(separator: "\n")
+        }
+        
+        return nil
+    }
+    
 }
