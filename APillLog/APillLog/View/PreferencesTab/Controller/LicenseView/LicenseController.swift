762c708bd8c2b69928ebafd31f67d096247e7219
mycookie1 <103025266+mycookie1@users.noreply.github.com>
2022-08-01 01:04:24 +0900

    [Feat] #168 - 라이센스페이지 연결

--- APillLog/APillLog/View/PreferencesTab/Controller/LicenseView/LicenseController.swift
+++ APillLog/APillLog/View/PreferencesTab/Controller/LicenseView/LicenseController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class LicenseViewController: UIViewController {
     
+    @IBOutlet weak var fsButton: UIButton!
+    @IBOutlet weak var chartsButton: UIButton!
+    @IBOutlet weak var licenseTitle: UILabel!
+    
+    @IBAction func tapFSButton(){
+        let storyboard = UIStoryboard(name: "FSLicenseView", bundle: nil)
+        let vc =  storyboard.instantiateViewController(withIdentifier: "fsLicenseView") as! FSLicenseViewController
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    @IBAction func tapChartsButton(){
+        let storyboard = UIStoryboard(name: "ChartsLicenseView", bundle: nil)
+        let vc =  storyboard.instantiateViewController(withIdentifier: "ChartsLicenseView") as! ChartsLicenseViewController
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.AColor.background
         self.navigationController?.navigationBar.tintColor = UIColor.AColor.accent
+        self.navigationController?.title = "설정"
         self.navigationController?.navigationBar.topItem?.title = "뒤로"
+        self.fsButton.layer.cornerRadius = 14
+        self.chartsButton.layer.cornerRadius = 14
+        self.licenseTitle.font = UIFont.AFont.navigationTitle
     }
 }
